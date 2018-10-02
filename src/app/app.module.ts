3ced75e9c3785abf6d927c2e809f3205d3d2590c
Gabriela Mancini <gabriela.mancini@darwoft.com>
2018-10-01 22:52:26 -0300

    App Module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeadertopComponent } from './shared/headertop/headertop.component';
 import { HeaderbottomComponent } from './shared/headerbottom/headerbottom.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FooterComponent } from './shared/footer/footer.component'; 
+import { AboutComponent } from './pages/about/about.component'; 
+import { PostComponent } from './pages/post/post.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+//Rutas
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadertopComponent,
     HeaderbottomComponent,
+    FooterComponent,
     HomeComponent,
-    FooterComponent 
+    AboutComponent,
+    PostComponent,
+    ContactComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
