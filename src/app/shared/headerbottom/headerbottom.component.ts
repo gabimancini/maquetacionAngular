2ad31ed3c61541700ed083f8d1e028e83d3ceae9
Gabriela Mancini <gabriela.mancini@darwoft.com>
2018-10-03 16:32:29 -0300

    Modificación nombres de variable img fondo

--- src/app/shared/headerbottom/headerbottom.component.ts
+++ src/app/shared/headerbottom/headerbottom.component.ts
@@ -7,12 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderbottomComponent implements OnInit {
 	homeImg = "url(../../assets/images/home-bg.jpg)";
-	about = "url(../../assets/images/about-bg.jpg)";
-	post = "url(../../assets/images/post-bg.jpg)";
-	contact = "url(../../assets/images/contact-bg.jpg)"; 
+	aboutImg = "url(../../assets/images/about-bg.jpg)";
+	postImg = "url(../../assets/images/post-bg.jpg)";
+	contactImg = "url(../../assets/images/contact-bg.jpg)"; 
  
-
-
   ngOnInit() { 
   }
 
