8644aae873e38936029bd5cb8d0c548706f17a30
Gabriela Mancini <gabriela.mancini@darwoft.com>
2018-10-04 10:44:23 -0300

    Cambio de img fondo según rutas

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  template: "<div>cecrrf</div>"
 })
 export class AboutComponent implements OnInit {
 
--- src/app/shared/headerbottom/headerbottom.component.ts
+++ src/app/shared/headerbottom/headerbottom.component.ts
@@ -1,17 +1,54 @@
-import { Component, OnInit } from '@angular/core'; 
-
+import { Component, OnInit } from "@angular/core";
+// Importamos router de Angular
+import { Router, NavigationEnd } from "@angular/router";
 @Component({
-  selector: 'app-headerbottom',
-  templateUrl: './headerbottom.component.html',
-  styleUrls: ['./headerbottom.component.css']
+  selector: "app-headerbottom",
+  templateUrl: "./headerbottom.component.html",
+  styleUrls: ["./headerbottom.component.css"]
 })
 export class HeaderbottomComponent implements OnInit {
-	homeImg = "url(../../assets/images/home-bg.jpg)";
-	aboutImg = "url(../../assets/images/about-bg.jpg)";
-	postImg = "url(../../assets/images/post-bg.jpg)";
-	contactImg = "url(../../assets/images/contact-bg.jpg)"; 
- 
-  ngOnInit() { 
+  bakcgroundImage: String;
+  // Inyectamos router en el constructor
+  constructor(public route: Router) {
+    // Luego de inyectarlo en el constructor, creamos un metodo para suscribirse a los cambios de ruta.
+    // Este método dispone de muchos eventos, ver: https://angular.io/api/router/Event
+    this.route.events.subscribe(currentRouteData => {
+      // En este caso nos importará el evento NavigationEnd, por lo que solo trabajaremos con ese
+      // El mismo se dispará cuando la navegación finalizó su proceso
+      // El if es necesario para obtener solamente la instancia de NavigationEnd y no todas las que se disparan
+      if (currentRouteData instanceof NavigationEnd) {
+        // Hacemos un log en consola para ver datos de esta instancia
+        console.log("NavigationEnd:", currentRouteData.url);
+        // Aplicamos la lógica correspondiente según queramos;
+        this.changeHeaderData(currentRouteData.url);
+      }
+    });
   }
 
+  ngOnInit() {}
+
+  changeHeaderData(url) {
+    const homeImg: String = "../../assets/images/home-bg.jpg";
+    const aboutImg: String = "../../assets/images/about-bg.jpg";
+    const postImg: String = "../../assets/images/post-bg.jpg";
+    const contactImg: String = "../../assets/images/contact-bg.jpg";
+
+    switch (url) {
+      case "/home":
+        this.bakcgroundImage = homeImg;
+        break;
+      case "/about":
+        this.bakcgroundImage = aboutImg;
+        break;
+      case "/post":
+        this.bakcgroundImage = postImg;
+        break;
+      case "/contact":
+        this.bakcgroundImage = contactImg;
+        break;
+      default:
+        this.bakcgroundImage = contactImg;
+        break;
+    }
+  }
 }
